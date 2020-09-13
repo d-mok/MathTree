5c823d818396afb9c27cfae3b48dd7ee543ea339
d-mok <49301824+d-mok@users.noreply.github.com>
2020-09-13 23:56:07 +0800

    up

--- src/Pen/AutoPen.ts
+++ src/Pen/AutoPen.ts
@@ -174,6 +174,183 @@ class AutoPenCls {
         pen.autoCrop();
         this.pen = pen;
     }
+
+
+
+
+    /**
+     * Trig Graph for solving basic trig equation.
+     * @memberof AutoPen.AutoPen_Tool
+     * @param {string} trig - 'sin' | 'cos' | 'tan'
+     * @param {number} k - value of trig, like sin = k.
+     * @param {number} [scale=0.8] - scale for pen.setup.size()
+     * @param {number} [ratio=0.7] - ratio for pen.setup.size()
+     * @returns {void} The image is ready for export.
+     * @example
+     * autoPen.TrigSolution({trig:'sin', k=0.5})
+     */
+    TrigSolution({ trig = 'sin', k = 0, scale = 0.8, ratio = 0.7 }: { trig: string, k: number, scale: number, ratio: number }) {
+
+        if (trig === 'sin' || trig === 'cos') {
+            if (k > 2) k = 2
+            if (0.9 < k && k < 1) k = 0.9
+            if (0 < k && k < 0.3) k = 0.3
+            if (-1 < k && k < -0.9) k = -0.9
+            if (-0.3 < k && k < 0) k = -0.3
+            if (k < -2) k = -2
+        }
+
+        if (trig === 'tan') {
+            if (k > 4) k = 4
+            if (k < -4) k = -4
+            if (0 < k && k < 0.9) k = 0.9
+            if (0 > k && k > -0.9) k = -0.9
+        }
+
+        let [a, b, c] = TrigRoot(trig, k)
+
+
+        const pen = new Pen()
+        pen.setup.size(scale, ratio)
+
+        let limit = Max(1, Abs(k)) + 0.2
+
+        if (trig === 'sin') pen.setup.range([-40, 390], [-limit, limit])
+        if (trig === 'cos') pen.setup.range([-40, 390], [-limit, limit])
+        if (trig === 'tan') pen.setup.range([-40, 390], [-5, 5])
+
+        pen.axis.x()
+        pen.axis.y()
+        if (trig === 'sin' || trig === 'cos') {
+            pen.tick.x(360)
+        }
+        if (trig === 'tan') {
+            pen.tick.x(180)
+        }
+
+        if (trig === 'sin' || trig === 'cos') {
+            pen.cutterV([0, 1])
+            pen.cutterV([0, -1])
+            pen.label([0, 1], '1', 180)
+            pen.label([0, -1], '-1', 180)
+        }
+
+
+        pen.set.weight(1.5)
+        if (trig === 'sin') pen.plot(x => sin(x), 0, 360)
+        if (trig === 'cos') pen.plot(x => cos(x), 0, 360)
+        if (trig === 'tan') {
+            pen.plot(x => tan(x), 0, 360)
+            pen.set.strokeColor('grey')
+            pen.set.dash([5, 10])
+            pen.set.weight(0.7)
+            pen.straight.vertical(90)
+            pen.straight.vertical(270)
+            pen.set.strokeColor()
+            pen.set.dash()
+            pen.set.weight(1)
+        }
+        pen.set.weight(1)
+
+
+        function arrow(x: number, y: number, func: string, label = '') {
+            let anchor = 0
+            let skipAnchor = false
+
+            if (func === 'sin') {
+                if ([0, 90, 180, 270, 360].includes(x)) skipAnchor = true
+                if (x > 0 && x < 90) anchor = 0
+                if (x > 90 && x < 270) anchor = 180
+                if (x > 270 && x < 360) anchor = 360
+            }
+
+            if (func === 'cos') {
+                if ([0, 90, 180, 270, 360].includes(x)) skipAnchor = true
+                if (x > 0 && x < 180 && x !== 90) anchor = 0
+                if (x > 180 && x < 360 && x !== 270) anchor = 360
+            }
+
+            if (func === 'tan') {
+                if ([0, 90, 180, 270, 360].includes(x)) skipAnchor = true
+                if (x > 0 && x < 180) anchor = 0
+                if (x > 180 && x < 360) anchor = 180
+            }
+
+            if (x === undefined) return
+            let P = [x, y]
+            let Q = [x, 0]
+            let R = [anchor, 0]
+            pen.set.fillColor()
+            pen.point(P)
+            pen.set.fillColor('red')
+            if (y !== 0) { pen.line(P, Q, true) }
+            if (y >= 0) { pen.label(Q, label, 270) }
+            if (y < 0) { pen.label(Q, label, 90) }
+
+            if (skipAnchor) return
+            pen.set.weight(3)
+            pen.set.strokeColor('blue')
+            pen.line(R, Q)
+            pen.set.weight(1)
+            pen.set.strokeColor('red')
+        }
+
+        pen.set.strokeColor('red')
+        pen.set.fillColor('red')
+        pen.set.dash([5, 5])
+        pen.straight.horizontal(k)
+        pen.set.dash()
+
+        if (trig === 'sin') {
+            if (k === 0) {
+                arrow(a, k, 'sin', '0')
+                arrow(b, k, 'sin', '180')
+                arrow(c, k, 'sin', '360')
+            }
+            if (k === 1) {
+                arrow(a, k, 'sin', '90')
+            }
+            if (k === -1) {
+                arrow(a, k, 'sin', '270')
+            }
+            if (k > -1 && k < 1 && k !== 0) {
+                arrow(a, k, 'sin', 'α')
+                arrow(b, k, 'sin', 'β')
+            }
+        }
+
+        if (trig === 'cos') {
+            if (k === 0) {
+                arrow(a, k, 'cos', '90')
+                arrow(b, k, 'cos', '270')
+            }
+            if (k === 1) {
+                arrow(a, k, 'cos', '0')
+                arrow(b, k, 'cos', '360')
+            }
+            if (k === -1) {
+                arrow(a, k, 'cos', '180')
+            }
+            if (k > -1 && k < 1 && k !== 0) {
+                arrow(a, k, 'cos', 'α')
+                arrow(b, k, 'cos', 'β')
+            }
+        }
+
+        if (trig === 'tan') {
+            if (k === 0) {
+                arrow(a, k, 'tan', '0')
+                arrow(b, k, 'tan', '180')
+                arrow(c, k, 'tan', '360')
+            }
+            if (k !== 0) {
+                arrow(a, k, 'tan', 'α')
+                arrow(b, k, 'tan', 'β')
+            }
+        }
+
+        this.pen = pen;
+    }
 }
 
 
