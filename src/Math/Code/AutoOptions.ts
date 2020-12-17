d7686b97c1057bc33527cb9907283b62cfb9ef6b
d-mok <49301824+d-mok@users.noreply.github.com>
2020-12-17 20:47:13 +0800

    up

--- src/Math/Code/AutoOptions.ts
+++ src/Math/Code/AutoOptions.ts
@@ -22,22 +22,22 @@ class Instruction {
 
 
 
-function ValidateProducts(products: Partial<Dict>, source: Dict, validate: string) {
-    if (validate === "") return;
-    validate = validate.replace('\n', ' ');
-    for (let index = 0; index < 3; index++) {
-        let clone = Clone(source)
-        for (let key in products) {
-            clone[key] = products[key][index]
-        }
-        let {
-            a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
-            A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
-        } = clone;
-        if (eval(validate) === false)
-            throw "validate fail"
-    }
-}
+// function ValidateProducts(products: Partial<Dict>, source: Dict, validate: string) {
+//     if (validate === "") return;
+//     validate = validate.replace('\n', ' ');
+//     for (let index = 0; index < 3; index++) {
+//         let clone = Clone(source)
+//         for (let key in products) {
+//             clone[key] = products[key][index]
+//         }
+//         let {
+//             a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
+//             A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+//         } = clone;
+//         if (eval(validate) === false)
+//             throw "validate fail"
+//     }
+// }
 
 
 
@@ -47,22 +47,13 @@ type Product = {
 
 
 
-/**
-* @category AutoOptions
-* @return append the array of options to question
-* ```typescript
-* let question = 'abc<ul><li>*x</li></ul>'
-* AutoOptions(question,{x:3}) 
-* // 'abc<ul><li>*x</li><li>2</li><li>4</li><li>5</li></ul>'
-* ```
-*/
 function ExecInstructions(instructions: Partial<Dict>, source: Dict, validate: string): Product {
     let products: Product = {}
     for (let k in instructions) {
         let instr = new Instruction(instructions[k])
         products[k] = instr.do(source[k])
     }
-    ValidateProducts(products, source, validate)
+    // ValidateProducts(products, source, validate)
     return products
 }
 globalThis.ExecInstructions = ExecInstructions
@@ -88,13 +79,11 @@ function AutoOptions(instructions: Partial<Dict>, question: string, source: Dict
     let others = Array(3).fill(options[0])
 
     let products = ExecInstructions(instructions, source, validate)
-
     for (let k in products) {
         for (let i = 0; i < 3; i++) {
             others[i] = PrintVariable(others[i], k, products[k][i])
         }
     }
-
     return AppendInHTMLTag(question, 'ul', JoinToHTMLTag(others, 'li'))
 }
 globalThis.AutoOptions = AutoOptions
