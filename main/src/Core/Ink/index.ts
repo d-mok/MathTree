a8ebcd299a75255f65eb01b0a7af11658cb1053a
d-mok <49301824+d-mok@users.noreply.github.com>
2022-10-22 23:15:57 +0800

    up

--- main/src/Core/Ink/index.ts
+++ main/src/Core/Ink/index.ts
@@ -253,5 +253,5 @@ export function printOrdinal(n: number): string {
     if (j === 1 && k !== 11) return n + 'st'
     if (j === 2 && k !== 12) return n + 'nd'
     if (j === 3 && k !== 13) return n + 'rd'
-    return n + 'th'
+    return cal.blur(n) + 'th'
 }
