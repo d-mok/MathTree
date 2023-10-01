1da451013b6fdae7c4a737279aa09d8154000c5f
d-mok <49301824+d-mok@users.noreply.github.com>
2023-10-01 12:31:37 +0800

    up

---
+++ main/modify_declaration.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const declarationFilePath = '../dist/type/declaration.d.ts'
+
+fs.readFile(declarationFilePath, 'utf8', (err, data) => {
+    if (err) return console.log(err)
+
+    data = data.replaceAll(
+        'type Point2D = [x: number, y: number];',
+        'type Point2D = [x: number, y: number] | number[];'
+    )
+
+    data = data.replaceAll(
+        'type Point3D = [x: number, y: number, z: number];',
+        'type Point3D = [x: number, y: number, z: number] | number[];'
+    )
+
+    fs.writeFile(declarationFilePath, data, 'utf8', err => {
+        if (err) return console.log(err)
+    })
+})
