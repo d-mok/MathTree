b9f27ab3b868e395f1c16087281bb2144a203208
d-mok <49301824+d-mok@users.noreply.github.com>
2024-01-18 22:27:20 +0800

    up

--- main/src/Soil/soil.ts
+++ main/src/Soil/soil.ts
@@ -125,6 +125,7 @@ export class Soil {
                 if (e instanceof Error) {
                     switch (e.name) {
                         case 'ContractError':
+                            throw e // testing
                             this.logger.add(e)
                             break
                         case 'MathError':
