cdc5e49c4ae0d351c256bf3819149eddda018ab6
d-mok <49301824+d-mok@users.noreply.github.com>
2025-04-01 22:04:17 +0800

    up

--- main/src/Soil/soil.ts
+++ main/src/Soil/soil.ts
@@ -1,7 +1,8 @@
 import { shuffleOptions } from './tool/shuffle.js'
 import { AutoOptions } from './tool/option.js'
 import { Config } from './cls.js'
-import renderMathInElement from 'katex/dist/contrib/auto-render'
+//@ts-ignore
+import renderMathInElement from 'katex/dist/contrib/auto-render.js'
 import { dress, evalCtx, exprCtx, cropSection, loopSection, Timer } from 'bot'
 import { blacksmith } from './tool/blacksmith.js'
 
